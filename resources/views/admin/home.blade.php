f124e44ffa749f5e7228e768c7123bd781fdcf05
Arjun <jaluarjun244@gmail.com>
2023-03-08 15:46:52 +0530

    admin home test

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -14,7 +14,7 @@
                     </div>
                     @endif
 
-                    {{ __('Welcome - You are logged in to dashboard!') }}
+                    {{ __('Welcome - You are logged in !') }}
                 </div>
             </div>
         </div>

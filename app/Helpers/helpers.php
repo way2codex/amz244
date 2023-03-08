80e2347744ee3cc7dfd34ccbab2fdc839c0fb4c2
Arjun <jaluarjun244@gmail.com>
2023-03-08 15:13:07 +0530

    amz first commit

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,30 @@
+<?php
+
+use Carbon\Carbon;
+use App\Models\Category;
+use App\Models\Article;
+use App\Models\Store;
+
+if (!function_exists('all_category')) {
+    function all_category()
+    {
+        $all_category_data = Category::get();
+        return $all_category_data;
+    }
+}
+if (!function_exists('popular_article')) {
+    function popular_article($limit)
+    {
+        $popular_article_data = Article::limit($limit)->inRandomOrder()->get();
+        return $popular_article_data;
+    }
+}
+if (!function_exists('store_data')) {
+    function store_data()
+    {
+        $store_id = env('STORE_ID',0);
+        $store_data = Store::where('id',$store_id)->first();
+        return $store_data;
+    }
+}
+

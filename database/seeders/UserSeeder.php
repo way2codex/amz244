0029487ecf4ad0ccfa9380640a3f7c834458cca1
Arjun <jaluarjun244@gmail.com>
2023-03-12 19:05:53 +0530

    upcoming exam temp commit

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        echo 'email->'.env('ADMIN_EMAIL');
         User::firstOrCreate([
             'name' => 'Admin',
             'email' => env('ADMIN_EMAIL'),
